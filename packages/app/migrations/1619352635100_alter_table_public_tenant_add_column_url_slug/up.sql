202f85b42854426eb189ab034c09955018860e72
Nahum Wild <nahum@opstrace.com>
2021-04-26 00:46:26 +1200

    ui: re-enambled tenant.url_slug migrationss
    Signed-off-by: Nahum Wild <nahum@opstrace.com>

--- packages/app/migrations/1619352635100_alter_table_public_tenant_add_column_url_slug/up.sql
+++ packages/app/migrations/1619352635100_alter_table_public_tenant_add_column_url_slug/up.sql
@@ -1,5 +1,5 @@
--- ALTER TABLE "public"."tenant" ADD COLUMN "url_slug" text NULL;
--- ALTER TABLE "public"."tenant" ADD CONSTRAINT "tenant_url_slug_key" UNIQUE ("url_slug");
+ALTER TABLE "public"."tenant" ADD COLUMN "url_slug" text NULL;
+ALTER TABLE "public"."tenant" ADD CONSTRAINT "tenant_url_slug_key" UNIQUE ("url_slug");
 
--- -- There are already DNS records using the tenant name, so need to keep those working
--- UPDATE "public"."tenant" t SET url_slug = t.name;
+-- There are already DNS records using the tenant name, so need to keep those working
+UPDATE "public"."tenant" t SET url_slug = t.name;
--- packages/app/migrations/1619352635101_create_url_slug_functions/up.sql
+++ packages/app/migrations/1619352635101_create_url_slug_functions/up.sql
@@ -1,41 +1,41 @@
--- -- source: https://www.kdobson.net/2019/ultimate-postgresql-slug-function/
+-- source: https://www.kdobson.net/2019/ultimate-postgresql-slug-function/
 
--- CREATE EXTENSION IF NOT EXISTS "unaccent";
+CREATE EXTENSION IF NOT EXISTS "unaccent";
 
--- CREATE OR REPLACE FUNCTION create_url_slug("value" TEXT)
--- RETURNS TEXT AS $$
---   -- removes accents (diacritic signs) from a given string --
---   WITH "unaccented" AS (
---     SELECT unaccent("value") AS "value"
---   ),
---   -- lowercases the string
---   "lowercase" AS (
---     SELECT lower("value") AS "value"
---     FROM "unaccented"
---   ),
---   -- remove single and double quotes
---   "removed_quotes" AS (
---     SELECT regexp_replace("value", '[''"]+', '', 'gi') AS "value"
---     FROM "lowercase"
---   ),
---   -- replaces anything that's not a letter, number, hyphen('-'), or underscore('_') with a hyphen('-')
---   "hyphenated" AS (
---     SELECT regexp_replace("value", '[^a-z0-9\\-_]+', '-', 'gi') AS "value"
---     FROM "removed_quotes"
---   ),
---   -- trims hyphens('-') if they exist on the head or tail of the string
---   "trimmed" AS (
---     SELECT regexp_replace(regexp_replace("value", '\-+$', ''), '^\-', '') AS "value"
---     FROM "hyphenated"
---   )
---   SELECT "value" FROM "trimmed";
--- $$ LANGUAGE SQL STRICT IMMUTABLE;
+CREATE OR REPLACE FUNCTION create_url_slug("value" TEXT)
+RETURNS TEXT AS $$
+  -- removes accents (diacritic signs) from a given string --
+  WITH "unaccented" AS (
+    SELECT unaccent("value") AS "value"
+  ),
+  -- lowercases the string
+  "lowercase" AS (
+    SELECT lower("value") AS "value"
+    FROM "unaccented"
+  ),
+  -- remove single and double quotes
+  "removed_quotes" AS (
+    SELECT regexp_replace("value", '[''"]+', '', 'gi') AS "value"
+    FROM "lowercase"
+  ),
+  -- replaces anything that's not a letter, number, hyphen('-'), or underscore('_') with a hyphen('-')
+  "hyphenated" AS (
+    SELECT regexp_replace("value", '[^a-z0-9\\-_]+', '-', 'gi') AS "value"
+    FROM "removed_quotes"
+  ),
+  -- trims hyphens('-') if they exist on the head or tail of the string
+  "trimmed" AS (
+    SELECT regexp_replace(regexp_replace("value", '\-+$', ''), '^\-', '') AS "value"
+    FROM "hyphenated"
+  )
+  SELECT "value" FROM "trimmed";
+$$ LANGUAGE SQL STRICT IMMUTABLE;
 
--- CREATE FUNCTION public.set_url_slug_from_name() RETURNS trigger
---     LANGUAGE plpgsql
---     AS $$
--- BEGIN
---   NEW.url_slug := create_url_slug(NEW.name);
---   RETURN NEW;
--- END
--- $$;
+CREATE FUNCTION public.set_url_slug_from_name() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+  NEW.url_slug := create_url_slug(NEW.name);
+  RETURN NEW;
+END
+$$;
